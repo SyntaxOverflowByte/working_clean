170e98fbf3642a6ed945609f8f0e20e20e244449
Shaun <sdenhoff@gmail.com>
2017-07-06 22:06:41 -0400

    Added comment to tcpServer.py for tomorrow

--- tcpServer.py
+++ tcpServer.py
@@ -1,24 +1,26 @@
 import socket
 
 def Main():
-	host = '127.0.0.1'
-	port = 2069
+    #Include a random choice for the port    
+    host = '127.0.0.1'
+    port = 2069
+    print "Port: %d" % (port)
 	
-	s = socket.socket()
-	s.bind((host,port))
+    s = socket.socket()
+    s.bind((host,port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print('Connection from: ' + str(addr))
 	
-	s.listen(1)
-	c, addr = s.accept()
-	print('Connection from: ' + str(addr))
-	
-	while True:
-		data = c.recv(1024)
-		if not data:
-			break
-		print(' from connected user: ' + str(addr))
-		data = str(data).upper()
-		print('sending: ' + str(data))
-		c.send(data)
+    while True:
+	data = c.recv(1024)
+	if not data:
+		break
+	print(' from connected user: ' + str(addr))
+	data = str(data).upper()
+	print('sending: ' + str(data))
+	c.send(data)
 	
 	c.close()
 
